9f28a564bf2f59203b75004c5387b5e9f26addf4
Abdelhakim Bachar <a.bachar@hotmail.fr>
2015-01-04 23:05:09 +0100

    Create settings.sh

---
+++ settings.sh
@@ -0,0 +1,6 @@
+# Terminal
+#
+echo ""
+echo "Setting the Pro theme by default in Terminal.app"
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
