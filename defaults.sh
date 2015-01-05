66995ee13106c1150949d96d4573bbe8b9639c71
Abdelhakim Bachar <a.bachar@hotmail.fr>
2015-01-05 11:27:19 +0100

    Update defaults.sh

--- defaults.sh
+++ defaults.sh
@@ -1,15 +1,17 @@
 # Terminal
 #
-echo "Setting the Pro theme by default in Terminal.app"
+echo "Costumize Terminal..."
 defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
 defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
 
 # Finder
 #
-echo "Use list view in all Finder windows by default"
+echo "Costumize Finder..."
 defaults write com.apple.finder FXPreferredViewStyle Nlsv
 
 # Dock
 #
+echo "Costumize Dock"
+defaults write com.apple.dock orientation left
 defaults write com.apple.dock minimize-to-application -boolean true
 defaults write com.apple.dock expose-animation-duration -float 0.12
